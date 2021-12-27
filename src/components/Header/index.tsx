8f283c8d97755eb080d84199faa3a24398b94754
DarcioCarvalho <darcio.nuno@gmail.com>
2021-12-27 09:55:02 -0300

    Aplicação Concluída

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import Modal from 'react-modal'
+import logoImg from '../../assets/logo.svg'
+import { Container, Content } from './styles'
+
+interface headerProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: headerProps) {
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
+
+      </Content>
+    </Container>
+  )
+}
